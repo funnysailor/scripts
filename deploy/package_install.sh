a9f624be2fa5bb1d35eeb223067557ae0acad19c
funnysailor <buben4ik@gmail.com>
2016-05-15 23:07:29 +0300

    Update package_install.sh

--- deploy/package_install.sh
+++ deploy/package_install.sh
@@ -1,2 +1,2 @@
 echo "Install packages..."
-yum install screen htop ntp unzip iotop sysstat lsof strace atop daemonize gperftools-libs zabbix-agent parted xmlstarlet smartmontools iftop -y
+yum install screen htop ntp unzip iotop sysstat lsof strace atop daemonize gperftools-libs parted xmlstarlet smartmontools iftop -y
