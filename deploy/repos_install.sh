85a38ce80b309564fbb24ae12929a632b07e3709
funnysailor <buben4ik@gmail.com>
2016-05-15 23:07:01 +0300

    Update repos_install.sh

--- deploy/repos_install.sh
+++ deploy/repos_install.sh
@@ -3,7 +3,7 @@ echo "Add repos..."
 
 yum install wget -y
 
-wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
-rpm -Uvh http://repo.zabbix.com/zabbix/2.4/rhel/6/x86_64/zabbix-release-2.4-1.el6.noarch.rpm
+yum install epel-release
+
+
+
